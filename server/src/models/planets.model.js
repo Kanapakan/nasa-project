ddef62375d1e634cc602eb936ec2ef4ff6771c15
Kanapakan <56270434+Kanapakan@users.noreply.github.com>
2024-01-21 16:53:53 +0700

    init project

---
+++ server/src/models/planets.model.js
@@ -0,0 +1,55 @@
+const { parse } = require('csv-parse');
+const fs = require('fs');
+const path = require('path');
+
+const habitablePlanets = [];
+
+function isHabitablePlanets(planet) {
+	return planet['koi_disposition'] === 'CONFIRMED' 
+	&& planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+	&& planet['koi_prad'] < 1.6;
+}
+
+/*
+const promise = new Promise((resolve, reject) => {
+	resolve(42);
+});
+
+promise.then((result) => {
+
+});
+const result = await promise;
+console.log(result);
+*/
+
+function loadPlanetsData() {
+	return new Promise((resolve, reject) => {
+		fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+		.pipe(parse({
+			comment: '#',
+			columns: true,
+		}))
+		.on('data', data => {
+			if (isHabitablePlanets(data)) {
+				habitablePlanets.push(data)
+			}
+		})
+		.on('error', err => {
+			console.log(err);
+			reject();
+		})
+		.on('end', () => {
+			console.log(`${habitablePlanets.length} habitable planets found!`);
+			resolve();
+		});
+	});
+}
+
+function getAllPlanets() {
+	return habitablePlanets;
+}
+ 
+module.exports = {
+	loadPlanetsData,
+	getAllPlanets,
+};
